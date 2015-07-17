717ee65a0730d1d1eb914942f9bf6730ab4aec08
Samim Pezeshki <p.samim@gmail.com>
2015-07-17 21:55:45 +0430

    Modify init script for client

--- app/client.rb
+++ app/client.rb
@@ -1,14 +1,17 @@
 $:.unshift(File.dirname(__FILE__))
-require 'config'
 require 'rubygems'
 require 'data_mapper'
 require 'models/task'
 require 'narayana/transaction'
+require 'config'
 
-DataMapper.setup :default, "sqlite3://#{Dir.pwd}/db.db"
-DataMapper.finalize
-Task.auto_migrate!
 
 tx = Transaction.new
-t1 = Task.create name: 'Task One'
-tx.participate t1.url
+
+t = Task.create name: 'Task One'
+tx.participate t.url
+
+t = Task.create name: 'Task Two'
+tx.participate t.url
+
+tx.commit
